0f2d0779fa69d1e3531045886a8b62d8b7d26c28
Just1bridou <45208166+Just1bridou@users.noreply.github.com>
2021-06-17 21:30:38 +0200

    wip

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ var wsServer = new webSocketServer({
 
 var playersList = [];
 var roomsList = [];
+var questions = [];
 
 class Room {
     constructor(data, code = null) {
@@ -48,6 +49,30 @@ class Player {
         this.position = [0,0]
     }
 }
+
+class Question {
+    constructor(question, answer, comment) {
+      this.question = question;
+      this.answer = answer;
+      this.comment = comment;
+    }
+}
+
+let question1 = new Question("le Ciel est bleu à Annecy", true, "Lève les yeux");
+let question2 = new Question("le nom de la rivière qui traverse la ville est le Thiou ?",true, "Le Thiou la petite rivière de 3,5 km de long qui travèrse Annecy. Elle est le déversoir naturel du lac d'Annecy dans le Fier.");
+let question3 = new Question("l'arquebuse est l'alcool local d'Annecy ?",false,"C'est le génépi l’un des emblèmes de la Haute-Savoie, Plante rare qu’il est exclusivement possible de trouver en haute montagne (entre 2500 à 3200 mètres d’altitude)");
+let question4 = new Question("au dernier recensement le nombre d'habitants d'Annecy est 52 000 habitants ?",true,"La population légale 2018 pour Annecy était de 131 481 habitants");
+let question5 = new Question("Le nom du navire qui a coulé dans le lac d'Annecy le 12 mars 1971 est le France?", true,"C'est bien la carcasse du France qui repose à 30 mètres de profondeurs");
+let question6 = new Question( "La formation du lac d'Annecy remonte à 51 000 ans durant une ère glacière",false,"La formation du lac remonté à plus de 18 000 lors de la fonte des glaciers des alpes");
+let question7 = new Question("Le Palais de I'Île d'Annecy à autrefois été utilisé comme Prison",true,"En 1325 du à sa place stratégique");
+
+questions.push(question1);
+questions.push(question2);
+questions.push(question3);
+questions.push(question4);
+questions.push(question5);
+questions.push(question6);
+questions.push(question7);
 
 /*
 var testRoom = new Room("room01")
@@ -132,8 +157,17 @@ wsServer.on('request', function(request) {
                 break;
 
             case "newGPSPosition":
-                console.log(message.data)
+                // console.log(message.data)
                 refreshPlayerPosition(getRoom(message.data.code), message.data.player, message.data.position)
+                break;
+
+            case "newUserScore":
+                // console.log(message.data)
+                updateScorePlayer(message.data)
+                break;
+
+            case "newMonumentQuizz":
+                newMonumentQuizz(message.data)
                 break;
         }
     });
@@ -143,6 +177,52 @@ wsServer.on('request', function(request) {
     // TODO
     });
 });
+
+function updateScorePlayer(data) {
+    let player = getPlayer(data.room, data.id)
+    player.score += getNewScore(data.answer)
+}
+/**
+ * 
+ * TODO ADD DETECTION PLAYER GPS // MONUMENT GPS
+ */
+function newMonumentQuizz(data) {
+
+    // temporary
+    let rTemp = getRoom(data.room)
+    // end
+
+    let player = getPlayer(data.room, data.id)
+
+    let question = quesions[getRndInteger(0, quesions.length - 1)]
+
+    //var monument = {"monumentId": player.admin, "question": question}
+
+    var monument = {"monumentId": room.randomMonuments, "question": question}
+
+    var message = JSON.stringify({'action': 'newMonumentQuizz','data': monument});
+
+    if(player.connection != null) {
+        player.connection.sendUTF(message);
+    }
+}
+
+function getPlayer(code, id) {
+    let room = getRoom(code)
+    for(let player of room.playerList) {
+        if(player.id == id) {
+            return player
+        }
+    }
+}
+
+function getNewScore(answer) {
+    if(answer) {
+        return 100
+    }else {
+        return 0
+    }
+}
 
 function refreshPlayerPosition(room, player, position) {
 
@@ -303,4 +383,8 @@ function generateRoomToken() {
     firstPart = ("000" + firstPart.toString(36)).slice(-3);
     secondPart = ("000" + secondPart.toString(36)).slice(-3);
     return firstPart + secondPart;
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
 }
